b73c16e3d2bf1a595ba9b00c3d68035f8ebd35e0
Marco <marcolee27@icloud.com>
2025-07-29 23:18:26 +0800

    add data loader

---
+++ celeb_video_annotator/data/__init__.py
@@ -0,0 +1,7 @@
+"""
+Data loading and processing utilities
+"""
+
+from .loader import load_dataset, extract_dataset_zip, get_available_faces
+
+__all__ = ["load_dataset", "extract_dataset_zip", "get_available_faces"] 
